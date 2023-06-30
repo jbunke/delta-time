8d622be9e87d16c74d19e037130678b9f2f87cee
Jordan from Laptop <jordantb99@gmail.com>
2023-06-30 12:15:37 +0100

    * Bug fix

--- test/com/jordanbunke/delta_time/game_world/physics/collision/ExampleConcreteCollider.java
+++ test/com/jordanbunke/delta_time/game_world/physics/collision/ExampleConcreteCollider.java
@@ -26,6 +26,17 @@ public class ExampleConcreteCollider<E extends Vector<E>> extends WeightedCollid
         return position;
     }
 
+    @Override
+    public void handleCollision(
+            final double otherWeight, final E overlap,
+            final double collisionFactor
+    ) {
+        final double totalWeight = getWeight() + otherWeight;
+        final double proportion = getWeight() <= 0d ? 0d
+                : (otherWeight <= 0d ? 1d : otherWeight / totalWeight);
+        move(overlap.scale(-proportion * collisionFactor));
+    }
+
     public void move(final E displacement) {
         position = position.displace(displacement);
     }
