926bc13199fd468fc937f9b80f396a82a32313fe
Jordan Bunke <jordantb99@gmail.com>
2023-12-11 01:34:33 +0000

    ~ Fixed bug where alphabetical!! keys aren't picked up properly when CTRL is being pressed

--- src/com/jordanbunke/delta_time/events/Key.java
+++ src/com/jordanbunke/delta_time/events/Key.java
@@ -54,6 +54,8 @@ public enum Key {
             case 38 -> UP_ARROW;
             case 39 -> RIGHT_ARROW;
             case 40 -> DOWN_ARROW;
+            case 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+                    79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 -> Key.valueOf(String.valueOf((char) code));
             case 127 -> DELETE;
             default -> UNSUPPORTED;
         };
