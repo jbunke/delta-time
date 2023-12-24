53c62a9dd8ec6cf5611bb1711403d20a7e4fcfcc
Jordan Bunke <jordantb99@gmail.com>
2023-12-24 02:54:36 +0000

    + Added global status map

--- src/com/jordanbunke/delta_time/utility/DeltaTimeGlobal.java
+++ src/com/jordanbunke/delta_time/utility/DeltaTimeGlobal.java
@@ -6,12 +6,14 @@ import com.jordanbunke.delta_time.debug.Debugger;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public final class DeltaTimeGlobal {
     private static final String DT_CHANNEL = Constants.TITLE;
 
     private static final Debugger DEBUGGER = Debugger.createWithExistingChannels(
             generateChannelMap());
+    private static final Map<String, Object> STATUS_MAP = new HashMap<>();
 
     private static Map<String, DebugChannel> generateChannelMap() {
         Map<String, DebugChannel> channelMap = new HashMap<>();
@@ -21,5 +23,15 @@ public final class DeltaTimeGlobal {
 
     public static void print(final String message) {
         DEBUGGER.getChannel(DT_CHANNEL).printMessage(message);
+    }
+
+    public static Optional<Object> getStatusOf(final String code) {
+        return STATUS_MAP.containsKey(code)
+                ? Optional.of(STATUS_MAP.get(code))
+                : Optional.empty();
+    }
+
+    public static void setStatus(final String code, final Object value) {
+        STATUS_MAP.put(code, value);
     }
 }
