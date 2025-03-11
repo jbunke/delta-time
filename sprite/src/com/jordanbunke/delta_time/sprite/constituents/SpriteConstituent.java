a5f9401756a258a1f58d88819670d0954ed19183
Jordan Bunke <jordantb99@gmail.com>
2025-03-11 04:16:35 +0000

    > `SpriteConstituent` now designated as functional interface

--- sprite/src/com/jordanbunke/delta_time/sprite/constituents/SpriteConstituent.java
+++ sprite/src/com/jordanbunke/delta_time/sprite/constituents/SpriteConstituent.java
@@ -2,6 +2,7 @@ package com.jordanbunke.delta_time.sprite.constituents;
 
 import com.jordanbunke.delta_time.image.GameImage;
 
+@FunctionalInterface
 public interface SpriteConstituent<R> {
     GameImage getSprite(final R spriteID);
 }
