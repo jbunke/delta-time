2fc623b73a8ecceaa1e8406c8043b0db3f15efb1
Jordan Bunke <jordantb99@gmail.com>
2025-04-12 21:08:20 -0600

    + Added alternative build functions for different DeltaScript entry rules

--- script/src/com/jordanbunke/delta_time/scripting/Interpreter.java
+++ script/src/com/jordanbunke/delta_time/scripting/Interpreter.java
@@ -1,6 +1,10 @@
 package com.jordanbunke.delta_time.scripting;
 
+import com.jordanbunke.delta_time.scripting.ast.nodes.ASTNode;
+import com.jordanbunke.delta_time.scripting.ast.nodes.expression.ExpressionNode;
 import com.jordanbunke.delta_time.scripting.ast.nodes.function.HeadFuncNode;
+import com.jordanbunke.delta_time.scripting.ast.nodes.function.HelperFuncNode;
+import com.jordanbunke.delta_time.scripting.ast.nodes.statement.StatementNode;
 import com.jordanbunke.delta_time.scripting.ast.symbol_table.SymbolTable;
 import com.jordanbunke.delta_time.scripting.util.ScriptErrorLog;
 import com.jordanbunke.delta_time.scripting.util.ScriptVisitor;
@@ -13,7 +17,9 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Interpreter {
@@ -96,12 +102,83 @@ public class Interpreter {
         }
     }
 
-    private static boolean check(
-            final HeadFuncNode script,
+    public HelperFuncNode buildFunction(final String func) {
+        return build(func, ScriptParser::helper, ScriptVisitor::visitHelper);
+    }
+
+    public ExpressionNode buildExpression(final String expr) {
+        try {
+            final CharStream input = CharStreams.fromString(expr);
+
+            final ScriptLexer lexer = new ScriptLexer(input);
+            lexer.removeErrorListeners();
+
+            final ScriptParser parser = new ScriptParser(
+                    new CommonTokenStream(lexer));
+            parser.removeErrorListeners();
+
+            return (ExpressionNode) visitor.visit(parser.expr());
+        } catch (Exception e) {
+            ScriptErrorLog.fireError(
+                    ScriptErrorLog.Message.COULD_NOT_READ,
+                    TextPosition.N_A);
+            displayErrors();
+            return null;
+        }
+    }
+
+    public StatementNode buildStatement(final String stat) {
+        try {
+            final CharStream input = CharStreams.fromString(stat);
+
+            final ScriptLexer lexer = new ScriptLexer(input);
+            lexer.removeErrorListeners();
+
+            final ScriptParser parser = new ScriptParser(
+                    new CommonTokenStream(lexer));
+            parser.removeErrorListeners();
+
+            return (StatementNode) visitor.visit(parser.stat());
+        } catch (Exception e) {
+            ScriptErrorLog.fireError(
+                    ScriptErrorLog.Message.COULD_NOT_READ,
+                    TextPosition.N_A);
+            displayErrors();
+            return null;
+        }
+    }
+
+    private <N extends ASTNode, R> N build(
+            final String content,
+            final Function<ScriptParser, R> contextGetter,
+            final BiFunction<ScriptVisitor, R, N> fNode
+    ) {
+        try {
+            final CharStream input = CharStreams.fromString(content);
+
+            final ScriptLexer lexer = new ScriptLexer(input);
+            lexer.removeErrorListeners();
+
+            final ScriptParser parser = new ScriptParser(
+                    new CommonTokenStream(lexer));
+            parser.removeErrorListeners();
+
+            return fNode.apply(visitor, contextGetter.apply(parser));
+        } catch (Exception e) {
+            ScriptErrorLog.fireError(
+                    ScriptErrorLog.Message.COULD_NOT_READ,
+                    TextPosition.N_A);
+            displayErrors();
+            return null;
+        }
+    }
+
+    public static boolean check(
+            final ASTNode node,
             final SymbolTable scriptTable
     ) {
         try {
-            script.semanticErrorCheck(scriptTable);
+            node.semanticErrorCheck(scriptTable);
         } catch (Exception e) {
             errorHandling(false);
             return false;
