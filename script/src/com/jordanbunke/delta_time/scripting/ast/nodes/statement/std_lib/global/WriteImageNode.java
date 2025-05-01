feb56c66da9d66854eadceae4ec8fbce2b44e533
Jordan Bunke <jordantb99@gmail.com>
2025-05-01 02:51:46 -0600

    * Corrected `WriteImageNode::funcName`

--- script/src/com/jordanbunke/delta_time/scripting/ast/nodes/statement/std_lib/global/WriteImageNode.java
+++ script/src/com/jordanbunke/delta_time/scripting/ast/nodes/statement/std_lib/global/WriteImageNode.java
@@ -38,6 +38,6 @@ public final class WriteImageNode extends DefFuncExecNode {
 
     @Override
     protected String funcName() {
-        return "write_file";
+        return "write_image";
     }
 }
